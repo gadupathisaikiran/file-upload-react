1a7d3a84172e43fe4d875f892f0cabae854585e3
Gadupathi saikiran <saikirangaduparthi1@gmail.com>
2023-02-17 16:13:05 +0530

    code

---
+++ src/App.js
@@ -0,0 +1,28 @@
+
+import './App.css';
+import React, { useState } from "react";
+import { FileUploader } from "react-drag-drop-files";
+
+const fileTypes = ["JPG", "PNG", "GIF"];
+
+function App() {
+  const [file, setFile] = useState(null);
+  const handleChange = (file) => {
+    setFile(file);
+  };
+
+  console.log(file)
+  return (
+    <div style={{height:"500px"}} className="App">
+    <div className='App-header'>
+    <FileUploader handleChange={handleChange} name="file" types={fileTypes} />
+
+    <p>{file?file.name:""}</p>
+    </div>
+    
+    </div>
+   
+  );
+}
+
+export default App;
